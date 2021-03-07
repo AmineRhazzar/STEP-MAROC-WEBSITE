8f1a856896d0e3619a5f211c46451cb487051804
Mohammed El Amine Rhazzar <mohammedelaminerhazzar@gmail.com>
2021-03-07 06:22:07 +0100

    this shit again omg

--- pages/contact.php
+++ pages/contact.php
@@ -32,16 +32,16 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <header id="navbar" class="topnav">
           <div id="navbar-mobile">
-            <div id="logo"><img class="default" src="./STEP-logo-white.png" alt="">
-              <img class="animate" src="./STEP-logo.png" alt="">
-            </div>
+                <div id="logo"><img class="default" src="../images/STEP logo/STEP-logo-white.png" alt="">
+          <img class="animate" src="../images/STEP logo/STEP-logo.png" alt="">
+        </div>
             <div id="icon"> <a href="javascript:void(0);" class="icon" onclick="myFunction()">
               <i class="fa fa-bars"></i></a>
             </div>
           </div>
             <div class="mobile">
               <a href="./contact.php" class="item" id="active">Contact</a>
-              <a href="./about.html" class="item" >About</a>
+              <a href="./about.html" class="item" >Qui sommes-nous?</a>
               <a href="./capital.html" class="item">Capital</a>
               <a href="./domaines.html" class="item">Domaines</a>    
               <a href="../index.html" class="item">Acceuil</a>
@@ -49,24 +49,20 @@
         </header>
         <div class="opening">
           <div class="domaines-titre">
-            <h1>TITLE</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magnam iusto corrupti labore nihil rem earum nam quibusdam provident. Maxime eaque illo possimus sapiente voluptates sit neque, praesentium velit natus.</p>
+            <h1>CONTACTEZ-NOUS !</h1>
           </div>      
         </div>  
 
     <div class="container">
       <div class="adress">
         <div class="group">
-          <h3><i class="fas fa-envelope"></i>Email</h3>
-          <p>contact@stepmaroc.ma</p>
+          <h3><i class="fas fa-envelope"></i><span>Email : </span>contact@stepmaroc.ma</h3>
         </div>
         <div class="group">
-          <h3><i class="fas fa-phone"></i>Phone Number</h3>
-          <p>+212 522 226 290</p>
+          <h3><i class="fas fa-phone m"></i><span>Numéro de Téléphone : </span>+212 522 226 290</h3>
         </div>
         <div class="group">
-          <h3><i class="fas fa-map-marker-alt"></i>Adress</h3>
-          <p>223, Bd. Abdelmoumen Résidence Reda, N°2, Casablanca, Maroc.</p>
+          <h3><i class="fas fa-map-marker-alt"></i><span>Adresse : </span>223, Bd. Abdelmoumen Résidence Reda, N°2, Casablanca, Maroc.</h3>
           <div class="map-container">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13297.090707655436!2d-7.6257539!3d33.5722688!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xec989a5e92b13873!2sStep!5e0!3m2!1sfr!2sma!4v1613989423237!5m2!1sfr!2sma"
@@ -171,12 +167,12 @@
             </div>
           </div>
           <div class="footer-right col-md-4 col-sm-6">
-            <div id="logo2">          <img class="animate" src="./STEP-logo-white.png" alt="">
+            <div id="logo2">          <img class="animate" src="../images/STEP logo/STEP-logo-white.png" alt="">
             </div>                <p class="menu">
               <a href="../index.html"> Acceuil</a> |
               <a href="./domaines.html"> Domaines</a> |
               <a href="./capital.html"> Capital</a> |
-              <a href="./about.html"> About</a> |
+              <a href="./about.html"> Qui sommes-nous?</a> |
               <a href="./contact.php" id="active"> Contact</a> |
             </p>
             <p class="name"> STEP Maroc &copy; 2021</p>
